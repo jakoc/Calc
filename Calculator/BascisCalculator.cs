6bf6a04f2e5cddd40ffeed4c9c0fe4e71d6f2fcf
Jakoc <jacand01@easv365.dk>
2025-02-20 15:04:59 +0100

    Tilføjet duplicated lines måling til SonarQube

---
+++ Calculator/BascisCalculator.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator;
+
+public abstract class BascisCalculator : ICalculator
+{
+    public virtual int Add(int a, int b) => a + b;
+    public virtual int Subtract(int a, int b) => a - b;
+    public virtual int Multiply(int a, int b) => a * b;
+    public virtual int Divide(int a, int b) => a / b;
+    
+    public virtual int Factorial(int n)
+    {
+        if (n < 0) throw new ArgumentException("Factorial is not defined for negative numbers");
+        return (n == 0) ? 1 : n * Factorial(n - 1);
+    }
+
+    public virtual bool IsPrime(int candidate)
+    {
+        if (candidate < 2) return false;
+        for (int divisor = 2; divisor <= Math.Sqrt(candidate); ++divisor)
+        {
+            if (candidate % divisor == 0) return false;
+        }
+        return true;
+    }
+}
--- Calculator/CachedCalculator.cs
+++ Calculator/CachedCalculator.cs
@@ -2,87 +2,25 @@ using System.Runtime.CompilerServices;
 
 namespace Calculator;
 
-public class CachedCalculator : ICalculator
+public class CachedCalculator : BascisCalculator
 {
-    public readonly SimpleCalculator _calculator = new();
-    public readonly Dictionary<string, Calculation> _cache = new();
-    
-    public int Add(int a, int b)
+    private readonly Dictionary<string, object> _cache = new();
+
+    private T GetOrAddToCache<T>(Func<T> calculation, params object[] keys)
     {
-        var calc = GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Add(a, b), a, b);
-        return calc.Result;
+        string key = string.Join("_", keys);
+        if (_cache.TryGetValue(key, out var cachedValue))
+            return (T)cachedValue;
+
+        var result = calculation();
+        _cache[key] = result;
+        return result;
     }
 
-    public int Subtract(int a, int b)
-    {        
-        var calc = GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Subtract(a, b), a, b);
-        return calc.Result;
-    }
-
-    public int Multiply(int a, int b)
-    {        
-        var calc = GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Multiply(a, b), a, b);
-        return calc.Result;
-    }
-
-    public int Divide(int a, int b)
-    {        
-        var calc = GetCachedResult<int>(a, b) ?? StoreInCache(_calculator.Divide(a, b), a, b);
-        return calc.Result;
-    }
-
-    public int Factorial(int n)
-    {
-        var calc = GetCachedResult<int>(n) ?? StoreInCache(_calculator.Factorial(n), n);
-        return calc.Result;
-    }
-
-    public bool IsPrime(int candidate)
-    {
-        var calc = GetCachedResult<bool>(candidate) ?? StoreInCache(_calculator.IsPrime(candidate), candidate);
-        return calc.Result;
-    }
-    
-    private Calculation<T>? GetCachedResult<T>(int a, int? b = null, [CallerMemberName]string operation = "")
-    {
-        var calc = new Calculation<T>(default, operation, a, b);
-        if (_cache.ContainsKey(calc.GetKey()))
-        {
-            return (Calculation<T>?)_cache[calc.GetKey()];
-        }
-        return null;
-    }
-    
-    private Calculation<T> StoreInCache<T>(T result, int a, int? b = null, [CallerMemberName]string operation = "")
-    {
-        var calc = new Calculation<T>(result, operation, a, b);
-        calc.Result = result;
-        _cache.Add(calc.GetKey(), calc);
-        return calc;
-    }
-    
-    public class Calculation
-    {
-        private int A { get; }
-        private int? B { get; }
-        private string Operation { get; }
-
-        protected Calculation(string operation, int a, int? b = null)
-        {
-            A = a;
-            B = b;
-            Operation = operation;
-        }
-    
-        public string GetKey()
-        {
-            return string.Concat(A, Operation, B);
-        }
-    }
-
-    private class Calculation<T>(T? result, string operation, int a, int? b = null)
-        : Calculation(operation, a, b)
-    {
-        public T? Result { get; set; } = result;
-    }
+    public override int Add(int a, int b) => GetOrAddToCache(() => base.Add(a, b), nameof(Add), a, b);
+    public override int Subtract(int a, int b) => GetOrAddToCache(() => base.Subtract(a, b), nameof(Subtract), a, b);
+    public override int Multiply(int a, int b) => GetOrAddToCache(() => base.Multiply(a, b), nameof(Multiply), a, b);
+    public override int Divide(int a, int b) => GetOrAddToCache(() => base.Divide(a, b), nameof(Divide), a, b);
+    public override int Factorial(int n) => GetOrAddToCache(() => base.Factorial(n), nameof(Factorial), n);
+    public override bool IsPrime(int candidate) => GetOrAddToCache(() => base.IsPrime(candidate), nameof(IsPrime), candidate);
 }
